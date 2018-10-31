5295cdb2f7617a0f13f4892cf75859044a5b4b5d
starsProtector <starsprotector@163.com>
2018-10-31 16:21:46 +0800

    第一次提交代码,没有写任何东西

---
+++ app/src/main/java/com/sn/a1606bproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sn.a1606bproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
